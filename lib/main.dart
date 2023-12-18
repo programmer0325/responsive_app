6b2ea30b32298b93112fbdc75fb81b9aaa667bde
programmer0325 <programmmer0325@gmail.com>
2023-12-18 19:59:37 +0500

    hay!!

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_app/responsive_layout.dart';
+import 'package:responsive_app/screens/mobile_screen_layout.dart';
+import 'package:responsive_app/screens/web_screen_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  ResponsiveLayout(
+          mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout:  WebScreenLayout()),
+    );
+  }
+        }
